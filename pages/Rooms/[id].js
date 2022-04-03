b5e4f36e7bb7e62c2b1d790b79cc9bb51e2f104e
Vishnu Mallela <68802992+vishnumallela@users.noreply.github.com>
2022-04-03 22:03:11 +0530

    20mar

--- pages/Rooms/[id].js
+++ pages/Rooms/[id].js
@@ -5,36 +5,30 @@ import { ethers } from "ethers";
 import{ContractABI,ContractAddress} from '../../utils/constants'
 import Web3 from 'web3';
 import{BsFillPlusCircleFill} from 'react-icons/bs'
+import Image from "next/image";
 
 
 
 function Room() {
   const router = useRouter();
   const { id } = router.query;
-  
-  const[text,settext]= useState("Hello");
+
+  const [text, settext] = useState("Hello");
   const { user, Moralis } = useMoralis();
-  const [roomdetails, setroomdetails] = useState();
+  const [roomdetails, setroomdetails] = useState([]);
   const [provider, setProvider] = useState({});
-  const userName=user?.get("username");
-  const userEthadd = user?.get("ethAddress")
-  const[add,setadd]=useState();
+  const userName = user?.get("username");
+  const userEthadd = user?.get("ethAddress");
+  const [add, setadd] = useState();
 
   //Fetching live data of Room messages based on room id
-  
-  const { data, error, isLoading } = useMoralisQuery("Messages", query =>query.equalTo("roomID",id).ascending("createdAt"),[],{live:true},);
 
-  
-  
-
-  
-  
-
- 
-  
-
-
-
+  const { data, error, isLoading } = useMoralisQuery(
+    "Messages",
+    (query) => query.equalTo("roomID", id).ascending("createdAt"),
+    [],
+    { live: true }
+  );
 
   useEffect(async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -46,22 +40,16 @@ function Room() {
     setroomdetails(result);
   }, []);
 
+  //FUNCTION FOR SENDING MESSAGE TRANSACTION-   **IMPORTANT**  -
 
-//FUNCTION FOR SENDING MESSAGE TRANSACTION-   **IMPORTANT**  -
-
-const SendTransaction = async () => {
-    if(!text && !user) return null;
+  const SendTransaction = async () => {
+    if (!text && !user) return null;
     const signer = provider.getSigner();
 
     const amount = ethers.utils.parseEther("0");
     const contract = new ethers.Contract(ContractAddress, ContractABI, signer);
     const hash = await contract
-      .addToBlockchain(
-        userEthadd,
-        amount,
-        text,
-        id,
-      )
+      .addToBlockchain(userEthadd, amount, text, id)
       .then((hash) => {
         console.log(hash);
         const Messages = Moralis.Object.extend("Messages");
@@ -76,59 +64,79 @@ const SendTransaction = async () => {
   };
 
   const AddtoRoom = async () => {
-    if(!add) return null;
+    if (!add) return null;
     const ChatRooms = Moralis.Object.extend("ChatRooms");
     const query = new Moralis.Query(ChatRooms);
     query.equalTo("uid", id);
     const result = await query.first();
-    result.addUnique("people",add);
+    result.addUnique("people", add);
     result.save();
-    alert("User Added")
-
-
-
+    alert("User Added");
   };
 
+  return (
+    <div className="w-full h-[100vh] bg-white flex">
+      {roomdetails.length ? (
+        <>
+          <div className="w-[30%] h-full bg-blue-200">
+            <input
+              type="text"
+              onChange={(e) => setadd(e.target.value)}
+              placeholder="Enter UserName"
+              className="mt-10 ml-4 rounded-md py-2 pl-2 pr-40 focus:outline-none w-[20%]"
+            />
+            <button
+              onClick={AddtoRoom}
+              class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3"
+            >
+              Add <BsFillPlusCircleFill className="inline align-middle" />
+            </button>
 
+            <h1 className="font-bold ml-10 mt-3 tracking-wider">
+              PARTICIPANTS
+            </h1>
 
+            <div className="flex-col w-full ml-4 mt-4">
+              {roomdetails[0].attributes.people.map((item) => {
+                return (
+                  <>
+                    <div className="flex align-middle my-2 bg-white p-2 rounded-md w-[70%]">
+                      <Image
+                        className="object-contain p-5 inline-block rounded-full bg-yellow-400"
+                        src={`https://avatars.dicebear.com/api/avataaars/${item}.svg`}
+                        height="30px"
+                        width="30px"
+                      />
+                      <h1 className="ml-2 font-[Quantico]">{item}</h1>
+                    </div>{" "}
+                  </>
+                );
+              })}
+            </div>
+          </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-  
-    
-  return <div className='w-full h-[100vh] bg-white'>
-    <div className='w-[30%] h-full bg-blue-200'>
-      <input type="text" onChange={(e)=>setadd(e.target.value)} placeholder='Enter UserName' className='mt-10 ml-4 rounded-md py-2 pl-2 pr-40 focus:outline-none'/>
-      <button onClick={AddtoRoom} class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3">Add <BsFillPlusCircleFill className='inline align-middle'/>
-      </button>
-
-
-
-
-      </div>
-
-
-   
-
-
-
-
-   
-    
-  </div>;
+          <div className='bg-yellow-400 w-[70%] h-full'> 
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          </div>
+          
+        </>
+      ) : (
+        <h1>LOading...</h1>
+      )}
+    </div>
+  );
 }
 
 export default Room;
